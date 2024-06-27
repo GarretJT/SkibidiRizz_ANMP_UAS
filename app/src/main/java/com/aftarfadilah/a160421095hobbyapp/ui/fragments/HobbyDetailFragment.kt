a9228d356e1cf87bdfed0032a93b63ee5e2d1ac4
GarretJT <garrettanzil@gmail.com>
2024-06-27 18:30:12 +0700

    init project UTS

---
+++ app/src/main/java/com/aftarfadilah/a160421095hobbyapp/ui/fragments/HobbyDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.aftarfadilah.a160421095hobbyapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aftarfadilah.a160421095hobbyapp.R
+class HobbyDetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hobby_detail, container, false)
+    }
+}
---
+++ app/src/main/java/com/aftarfadilah/a160421095hobbyapp/ui/fragments/HomeFragment.kt
@@ -0,0 +1,78 @@
+package com.aftarfadilah.a160421095hobbyapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aftarfadilah.a160421095hobbyapp.databinding.FragmentHomeBinding
+import com.aftarfadilah.week4.viewmodel.ListViewModel
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val hobbyListAdapter  = HobbyListViewAdapter(arrayListOf())
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater,container, false)
+
+        binding.refreshLayout.setOnRefreshListener {
+            binding.recView.visibility = View.GONE
+            binding.txtError.visibility = View.GONE
+            binding.progressLoad.visibility = View.VISIBLE
+            viewModel.refresh()
+            binding.refreshLayout.isRefreshing = false
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = hobbyListAdapter
+        observeViewModel()
+
+    }
+    fun observeViewModel() {
+        viewModel.studentsLD.observe(viewLifecycleOwner, Observer {
+            hobbyListAdapter.updateStudentList(it)
+        })
+        viewModel.studentLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.txtError?.visibility = View.VISIBLE
+            } else {
+                binding.txtError?.visibility = View.GONE
+            }
+        })
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+
+    }
+}
