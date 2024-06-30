d61d274fc84648b3b4e0e46e62ce82ff6d07cd62
GarretJT <garrettanzil@gmail.com>
2024-06-30 18:23:01 +0700

    added Migration

--- app/src/main/java/com/aftarfadilah/a160421095hobbyapp/model/HobbyDatabase.kt
+++ app/src/main/java/com/aftarfadilah/a160421095hobbyapp/model/HobbyDatabase.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.Room
-import com.aftarfadilah.a160421095hobbyapp.util.DB_NAME
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.migration.Migration
 
-@Database(entities = [User::class, Hobby::class], version = 2) // Increment version number
+const val DB_NAME = "hobbydb"
+
+@Database(entities = [User::class, Hobby::class], version = 2)
 abstract class HobbyDatabase : RoomDatabase() {
+
     abstract fun userDao(): UserDao
     abstract fun hobbyDao(): HobbyDao
 
@@ -20,7 +24,8 @@ abstract class HobbyDatabase : RoomDatabase() {
                 context.applicationContext,
                 HobbyDatabase::class.java,
                 DB_NAME
-            ).build()
+            ).addMigrations(MIGRATION_1_2)
+                .build()
         }
 
         operator fun invoke(context: Context): HobbyDatabase {
@@ -28,5 +33,44 @@ abstract class HobbyDatabase : RoomDatabase() {
                 instance ?: buildDatabase(context).also { instance = it }
             }
         }
+    }
+}
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "CREATE TABLE IF NOT EXISTS `User_new` (" +
+                    "`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`firstName` TEXT, " +
+                    "`lastName` TEXT, " +
+                    "`password` TEXT, " +
+                    "`username` TEXT)"
+        )
+
+        database.execSQL(
+            "CREATE TABLE IF NOT EXISTS `Hobby_new` (" +
+                    "`uuid` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "`image` TEXT, " +
+                    "`title` TEXT, " +
+                    "`author` TEXT, " +
+                    "`desc` TEXT, " +
+                    "`dateCreated` TEXT)"
+        )
+
+        database.execSQL(
+            "INSERT INTO `User_new` (`id`, `firstName`, `lastName`, `password`, `username`)" +
+                    "SELECT `id`, `firstName`, `lastName`, `password`, `username` FROM `User`"
+        )
+
+        database.execSQL(
+            "INSERT INTO `Hobby_new` (`uuid`, `image`, `title`, `author`, `desc`)" +
+                    "SELECT `uuid`, `image`, `title`, `author`, `desc` FROM `Hobby`"
+        )
+
+        database.execSQL("DROP TABLE `User`")
+        database.execSQL("DROP TABLE `Hobby`")
+
+        database.execSQL("ALTER TABLE `User_new` RENAME TO `User`")
+        database.execSQL("ALTER TABLE `Hobby_new` RENAME TO `Hobby`")
     }
 }
